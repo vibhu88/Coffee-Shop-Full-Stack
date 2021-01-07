7d45f500b2b7cb740301bd718e8725a5aecdfd2e
vibhu88 <vibhu.eclipse@gmail.com>
2021-01-07 19:21:18 +0530

    Added Auth and Routes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-vu.us.auth0.com', // the auth0 domain prefix
+    audience: 'coffee-shop-backend', // the audience set for the auth0 app
+    clientId: '8JbmI4rqh4lyHQ7k1D0Zm5gRhjOGWuFQ', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
